cbf6a765643661149e5d0c586408ef1534d8a057
Jakub Brandt <jakub.brandt2002@gmail.com>
2024-10-23 22:31:52 +0200

    Dodałem na razie wizualnie przyciski

--- mainmenu.py
+++ mainmenu.py
@@ -1,7 +1,16 @@
 import pygame
 
-title_surface: pygame.Surface
+title_surf: pygame.Surface
 title_rect: pygame.Rect
+
+start_button_surf: pygame.Surface
+start_button_rect: pygame.Rect
+
+options_button_surf: pygame.Surface
+options_button_rect: pygame.Rect
+
+exit_button_surf: pygame.Surface
+exit_button_rect: pygame.Rect
 
 
 def update():
@@ -9,7 +18,30 @@ def update():
 
 
 def render(window: pygame.Surface):
-    title_surface = pygame.image.load("./Assets/MainMenu/Habit pet.png").convert_alpha()
-    title_rect = title_surface.get_rect(center=(640, 80))
+    global title_surf, title_rect
+    global start_button_surf, start_button_rect
+    global options_button_surf, options_button_rect
+    global exit_button_surf, exit_button_rect
 
-    window.blit(title_surface, title_rect)
+    title_surf = pygame.image.load("./Assets/MainMenu/Habit pet.png").convert_alpha()
+    title_rect = title_surf.get_rect(center=(640, 100))
+
+    start_button_surf = pygame.image.load(
+        "./Assets/MainMenu/Buttons/Start.png"
+    ).convert_alpha()
+    start_button_rect = start_button_surf.get_rect(center=(640, 300))
+
+    options_button_surf = pygame.image.load(
+        "./Assets/MainMenu/Buttons/Options.png"
+    ).convert_alpha()
+    options_button_rect = options_button_surf.get_rect(center=(640, 380))
+
+    exit_button_surf = pygame.image.load(
+        "./Assets/MainMenu/Buttons/Exit.png"
+    ).convert_alpha()
+    exit_button_rect = exit_button_surf.get_rect(center=(640, 460))
+
+    window.blit(title_surf, title_rect)
+    window.blit(start_button_surf, start_button_rect)
+    window.blit(options_button_surf, options_button_rect)
+    window.blit(exit_button_surf, exit_button_rect)
