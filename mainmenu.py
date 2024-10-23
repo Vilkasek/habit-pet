b3a9d5b2f21778354aa0c75bed751af6a53c589e
Jakub Brandt <jakub.brandt2002@gmail.com>
2024-10-23 22:00:56 +0200

    Usunąłem wesołe przechylanie się tytułu

--- mainmenu.py
+++ mainmenu.py
@@ -3,21 +3,9 @@ import pygame
 title_surface: pygame.Surface
 title_rect: pygame.Rect
 
-rotation_speed = 0.01
-direction = 1
-angle = 0
-max_angle = 3
-
 
 def update():
-    global rotation_speed
-    global angle
-    global direction
-
-    angle += rotation_speed * direction
-
-    if angle >= max_angle or angle <= -max_angle:
-        direction *= -1
+    pass
 
 
 def render(window: pygame.Surface):
@@ -25,8 +13,6 @@ def render(window: pygame.Surface):
     global title_rect
 
     title_surface = pygame.image.load("./Assets/MainMenu/Habit pet.png").convert_alpha()
-    title_surface = pygame.transform.rotate(title_surface, angle)
-
     title_rect = title_surface.get_rect(center=(640, 80))
 
     window.blit(title_surface, title_rect)
