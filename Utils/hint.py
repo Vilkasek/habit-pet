47dce9a38df24d9530da61dca8d7b29b835e3a07
Jakub Brandt <jakub.brandt2002@gmail.com>
2024-10-30 16:10:50 +0100

    Dodałem łagodniejsze pojawianie się i znikanie podpowiedzi

--- Utils/hint.py
+++ Utils/hint.py
@@ -5,13 +5,23 @@ class Hint:
     def __init__(self, image_path: str, position: tuple[int, int]):
         self.image: pygame.Surface = pygame.image.load(image_path)
         self.rect: pygame.Rect = self.image.get_rect(center=position)
+
         self.visible: bool = False
+        self.alpha: int = 0
+
+        self.image.set_alpha(self.alpha)
 
     def update(self, button_hovered: bool):
         if button_hovered:
-            self.visible = True
+            if self.alpha < 255:
+                self.alpha += 5
         else:
-            self.visible = False
+            if self.alpha > 0:
+                self.alpha -= 5
+
+        self.image.set_alpha(self.alpha)
+
+        self.visible = self.alpha > 0
 
     def draw(self, window: pygame.Surface):
         self.image = self.image.convert_alpha()
