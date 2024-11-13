0a4206695b0c0c58ebca393553ad587e63d2fae1
Jakub Brandt <jakub.brandt2002@gmail.com>
2024-11-13 16:21:58 +0100

    Dodałem animowane łapki w menu głównym

--- Utils/paws.py
+++ Utils/paws.py
@@ -1,44 +1,37 @@
+import os
+
 import pygame
 
 
 class Paw:
-    def __init__(
-        self, image_path: str, position: tuple[int, int], flipped: bool, delay: int = 0
-    ):
-        self.flipped = flipped
+    def __init__(self, folder_path: str, pos: tuple[int, int], framerate: int) -> None:
+        self.position = pos
 
-        if not self.flipped:
-            self.image: pygame.Surface = pygame.image.load(image_path)
-        else:
-            self.image: pygame.Surface = pygame.image.load(image_path)
-            self.image = pygame.transform.flip(self.image, True, False)
+        self.folder_path = folder_path
+        self.framerate = framerate
 
-        self.rect: pygame.Rect = self.image.get_rect(center=position)
-        self.alpha: int = 0
-        self.image.set_alpha(self.alpha)
+        self.frame_index = 0
+        self.frames = []
 
-        self.delay: int = delay
-        self.time_since_last_update: int = 0
-        self.fading_in: bool = True
+        self.last_update = pygame.time.get_ticks()
 
-    def update(self, dt: int):
-        self.time_since_last_update += dt
+        for filename in sorted(os.listdir(self.folder_path)):
+            if filename.endswith(".png"):
+                frame = pygame.image.load(os.path.join(folder_path, filename))
+                self.frames.append(frame)
 
-        if self.time_since_last_update >= self.delay:
-            if self.fading_in:
-                if self.alpha < 255:
-                    self.alpha += 2
-                else:
-                    self.fading_in = False
-            else:
-                if self.alpha > 0:
-                    self.alpha -= 2
-                else:
-                    self.fading_in = True
+    def update(self):
+        now = pygame.time.get_ticks()
 
-            self.image.set_alpha(self.alpha)
-            self.time_since_last_update = 0
+        if now - self.last_update > 1000 // self.framerate:
+            self.last_update = now
+            self.frame_index = (self.frame_index + 1) % len(self.frames)
 
     def draw(self, window: pygame.Surface):
-        self.image = self.image.convert_alpha()
-        window.blit(self.image, self.rect)
+        current_frame = self.frames[self.frame_index]
+        current_frame = pygame.transform.scale(current_frame, (400, 400))
+        current_frame = current_frame.convert_alpha()
+
+        frame_rect = current_frame.get_rect(center=self.position)
+
+        window.blit(current_frame, frame_rect)
