f329f175624524c6f89352a09e664b9bdceff0df
Jakub Brandt <jakub.brandt2002@gmail.com>
2024-11-18 20:20:53 +0100

    Dodałem w klasie menu głównego wyświetlanie przycisków ((na razie bez jakiejkolwiek funkcjonalności))

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import sys
 
 import pygame
 
+import main_menu
+
 
 class HabitPet:
     def __init__(self, size: tuple[int, int], title="Habit Pet"):
@@ -17,20 +19,31 @@ class HabitPet:
 
         self.clock = pygame.time.Clock()
 
-    def update(self):
+        self.main_menu = main_menu.MainMenu("./Assets/MainMenu/Habit pet.png")
+
+    def handle_event(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
 
+            self.main_menu.handle_events(event)
+
+    def update(self):
+        self.main_menu.update()
+
         self.clock.tick(60)
 
     def render(self):
         self.screen.fill((5, 31, 57))
+
+        self.main_menu.render(self.screen)
+
         pygame.display.flip()
 
     def run(self):
         while True:
+            self.handle_event()
             self.update()
             self.render()
 
--- main_menu.py
+++ main_menu.py
@@ -2,4 +2,42 @@ import pygame
 
 
 class MainMenu:
-    pass
+    def __init__(self, title_path: str) -> None:
+        self.title_path = title_path
+
+        self.title_surf = pygame.image.load(title_path)
+        self.title_rect = self.title_surf.get_rect(center=(1280 / 2, 100))
+
+        self.test_buttons_paths = [
+            "./Assets/MainMenu/Buttons/Start.png",
+            "./Assets/MainMenu/Buttons/Options.png",
+            "./Assets/MainMenu/Buttons/Exit.png",
+        ]
+
+        self.test_buttons_surfs = []
+
+        for path in self.test_buttons_paths:
+            self.test_buttons_surfs.append(pygame.image.load(path))
+
+        self.test_buttons_rects = [
+            self.test_buttons_surfs[0].get_rect(center=(1280 / 2, 300)),
+            self.test_buttons_surfs[1].get_rect(center=(1280 / 2, 380)),
+            self.test_buttons_surfs[2].get_rect(center=(1280 / 2, 460)),
+        ]
+
+    def handle_events(self, event: pygame.Event):
+        pass
+
+    def update(self):
+        pass
+
+    def render(self, screen: pygame.Surface):
+        self.title_surf = self.title_surf.convert_alpha()
+
+        for surf in self.test_buttons_surfs:
+            surf = surf.convert_alpha()
+
+        screen.blit(self.title_surf, self.title_rect)
+
+        for i in range(3):
+            screen.blit(self.test_buttons_surfs[i], self.test_buttons_rects[i])
