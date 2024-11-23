981f6a427c478a7e70a7d55efa7f54a6b2a148eb
Jakub Brandt <jakub.brandt2002@gmail.com>
2024-11-23 16:52:39 +0100

    Dodałem do menu zadań przycisk powrotu do menu głównego oraz przycisk, który będzie miał dodawać zadania

--- tracker_menu.py
+++ tracker_menu.py
@@ -1,5 +1,8 @@
 import pygame
 
+from utils.button import Button
+from utils.states import State
+
 
 class TrackerMenu:
     def __init__(self, title_path: str) -> None:
@@ -8,13 +11,28 @@ class TrackerMenu:
         self.title_surf = pygame.image.load(title_path)
         self.title_rect = self.title_surf.get_rect(center=(1280 / 2, 100))
 
+        self.mouse_pos = (0, 0)
+
+        self.buttons = [
+            Button("./Assets/TaskMenu/Controls/Back.png", (60, 660)),
+            Button("./Assets/TaskMenu/Controls/AddTaskButton.png", (640, 660)),
+        ]
+
+        self.state = State()
+
     def handle_events(self, event: pygame.Event):
-        pass
+        if self.buttons[0].is_clicked(event, self.mouse_pos):
+            self.state.change_state("main-menu")
+        if self.buttons[1].is_clicked(event, self.mouse_pos):
+            pass
 
     def update(self):
-        pass
+        self.mouse_pos = pygame.mouse.get_pos()
 
     def render(self, screen: pygame.Surface):
         self.title_surf.convert_alpha()
+
+        for button in self.buttons:
+            button.render(screen)
 
         screen.blit(self.title_surf, self.title_rect)
