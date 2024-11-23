54d08a42f8ff6a4d332ec2eb93a2ff16798f02e8
Jakub Brandt <jakub.brandt2002@gmail.com>
2024-11-23 18:39:12 +0100

    Dodałe do klasy task jej pola i podstawowe metody

--- tasks.py
+++ tasks.py
@@ -1,2 +1,24 @@
+import pygame
+
+
 class Task:
-    pass
+    def __init__(self, text: str, position: tuple[int, int]) -> None:
+        self.text = text
+        self.position = position
+        self.color = (255, 255, 255)
+
+        self.font = pygame.Font(None, 20)
+
+        self.text_surf = self.font.render(self.text, True, self.color)
+        self.text_rect = self.text_surf.get_rect(topright=(self.position))
+
+        self.checkboxes = []
+
+    def update(self):
+        for checkbox in self.checkboxes:
+            pass
+
+    def render(self, screen: pygame.Surface):
+        self.text_surf = self.text_surf.convert_alpha()
+
+        screen.blit(self.text_surf, self.text_rect)
