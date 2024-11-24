e38fd987320ec93733447fa4a450a7b39d612946
Jakub Brandt <jakub.brandt2002@gmail.com>
2024-11-24 14:38:44 +0100

    Dodałem pola klasy checkbox

--- utils/checkboxes.py
+++ utils/checkboxes.py
@@ -1,3 +1,18 @@
+import pygame
+
+
 class Checkbox:
-    # TODO: Dodać pola klasy i jej metody
-    pass
+    # TODO: Dodać jej metody
+    def __init__(self, position: tuple[int, int]) -> None:
+        self.position = position
+
+        self.images = [
+            pygame.image.load("../Assets/TaskMenu/Circles/CircleOff.png"),
+            pygame.image.load("../Assets/TaskMenu/Circles/CircleOn.png"),
+        ]
+        self.image_index = 0
+        self.image = self.images[self.image_index]
+
+        self.rect = self.image.get_rect(topright=self.position)
+
+        self.checked = False
