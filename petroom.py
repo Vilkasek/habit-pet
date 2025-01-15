0f9cf65c15c88c9763b95c7c7b8e5c410779290e
Vilkasek <109431561+Vilkasek@users.noreply.github.com>
2025-01-15 18:07:45 +0100

    Dodałem szkielet klasy pokoju stworzonka

---
+++ petroom.py
@@ -0,0 +1,14 @@
+import pygame
+
+class Petroom:
+    def __init__(self) -> None:
+        pass
+
+    def handle_events(self, event: pygame.Event):
+        pass
+
+    def update(self) -> None:
+        pass
+
+    def render(self, window: pygame.Surface) -> None:
+        pass
